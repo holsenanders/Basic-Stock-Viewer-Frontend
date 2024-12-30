a5793e822ccb7b24a7407705656e1106815b32ab
Anders Nikolai Holsen <andernho@stud.ntnu.no>
2024-12-30 23:03:29 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Searchbar from "./components/Searchbar.jsx";
+import StockGraph from "./components/StockGraph.jsx";
+import './App.css';
+
+
+function App() {
+    return (
+        <>
+            <Searchbar />
+            <StockGraph />
+        </>
+
+    );
+}
+
+export default App;
