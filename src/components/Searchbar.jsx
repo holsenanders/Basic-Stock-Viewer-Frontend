31aac3c91afc72471e1d4b499b231b0a13233253
Anders Nikolai Holsen <andernho@stud.ntnu.no>
2025-01-03 15:48:57 +0100

    changed symbol to stock

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -73,7 +73,7 @@ const Searchbar = () => {
         try {
             const response = await axios.get(`${BASE_URL}/api/stocks/get_data`, {
                 params: {
-                    symbol: stockTicker,
+                    stock: stockTicker,
                     start: startDate.toISOString().split("T")[0],
                     end: endDate,
                 },
